7748d6cc175fba466d18930828aa27e2cc7446c2
tzigri <tzihgri@gmail.com>
2022-12-04 14:14:36 +0200

    Updated names

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const http = require('http');
+//require module installed with npm
+const uc = require('upper-case');
 const fs = require('fs');
 const url = require('url');
 const port = process.env.port || 4000;
+
+
 http.createServer(function (req, res) {
-  let pathname = url.parse(req.url, true).pathname;
-  //handle root
-  if (pathname==="/") pathname="/index";
-  //add .html if not specified
-  if (!pathname.toLowerCase().endsWith(".html")) pathname+=".html";
-  let filename = __dirname + "/www" + pathname;
-  fs.readFile(filename, function (err, data) {
+  let result = uc.upperCase("testing upper-case module ");
+  let aFileName = __dirname + '/www/data/persons.json';
+  let nick = { name: "nick", age: 20 };
+  let mary = { name: "mary", age: 20 };
+  let aJsonString = JSON.stringify([nick, mary]);
+  //write to file
+  fs.writeFile(aFileName, aJsonString, function (err) {
     if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      return res.end("404 Not Found");
+      result += err.name + " " + err.massage;
+      res.end(result);
+  }
+    else {
+      result += "<div> File: <b>" + aFileName + "</b> created </div>";
+
+      //read from file
+      fs.readFile(aFileName, function (err, data) {
+        if (err) {
+          result += err.name + " " + err.massage;
+          res.end(result);
+        }
+        else {
+          let jsonData = JSON.parse(data);
+          result += "<div> File contents: " + JSON.stringify(jsonData) +"</div>";
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(result);
+        }
+      });
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
   });
+
 }).listen(port);
 console.log("Running at port " + port);
